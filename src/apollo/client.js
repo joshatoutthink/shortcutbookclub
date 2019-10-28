cbc06a9ca69a746eee81a3ac7f3c3805baf50976
Joshua Kennedy <josh@outthinkgroup.com>
2019-10-28 15:31:25 -0400

    fixed apollo client uri to use env var

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,10 +1,11 @@
 import ApolloClient from "apollo-boost"
 import fetch from "isomorphic-fetch"
+
 export const isBrowser = () => typeof window !== "undefined"
 const token = isBrowser() && window.localStorage.getItem("token")
 console.log(token)
 export const client = new ApolloClient({
-  uri: "http://shortcutbookclub.local/graphql",
+  uri: `${process.env.WP_URL}`,
   fetch,
   request: operation => {
     const token = localStorage.getItem("token")
